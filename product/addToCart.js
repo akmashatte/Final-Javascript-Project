28ed847794eb6437d9c2b55a14aff5cb24adc2e6
Avinash <87494977+akmashatte@users.noreply.github.com>
2022-03-20 23:03:45 +0530

    Add files via upload

---
+++ product/addToCart.js
@@ -0,0 +1,36 @@
+$(function () {
+  let getAllAddCardItems = JSON.parse(localStorage.getItem("addToCart"));
+  let checkOutHTML = "";
+  let totalAmount = 0;
+  for (let i = 0; i < getAllAddCardItems.length; i++) {
+    checkOutHTML += `<div id="addToCardItem">
+        <div>
+            <img class="checkoutItemImg" src="${getAllAddCardItems[i].img}"/>
+        </div>
+        <div>
+            <h4>${getAllAddCardItems[i].name}</h4>
+            <p class="noPerItem">x${getAllAddCardItems[i].total}</p>
+            <p class="AmountPerItem">Amount: Rs <span class="">${
+              getAllAddCardItems[i].total * getAllAddCardItems[i].price
+            }</span></p>
+        </div>
+    </div>`;
+    totalAmount += getAllAddCardItems[i].total * getAllAddCardItems[i].price;
+  }
+  $("#addToCartItems").append(checkOutHTML);
+  $("#addToCartTotalAmount").text(totalAmount);
+  $("#totalItemCount").text(getAllAddCardItems.length);
+
+  // Add to cart count
+  function addToCardCount() {
+    let total = 0;
+    let addToCartItemCount = localStorage.getItem("addToCart")
+      ? JSON.parse(localStorage.getItem("addToCart"))
+      : [];
+    for (let i = 0; i < addToCartItemCount.length; i++) {
+      total += addToCartItemCount[i].total;
+    }
+    $("#addCartCount").text(total);
+  }
+  addToCardCount();
+});
